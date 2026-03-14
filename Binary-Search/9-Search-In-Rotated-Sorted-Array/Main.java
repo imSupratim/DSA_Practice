2effa2de55d929fa95936f6e224a6b1533a25516
Supratim Mandal <supratimm98@gmail.com>
2026-03-14 10:45:54 +0530

    Search in rotated sorted array

---
+++ Binary-Search/9-Search-In-Rotated-Sorted-Array/Main.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+
+        while(low<=high){
+            int mid = low+(high - low)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        int result = solution.search(nums, target);
+        System.out.println("Index of " + target + " is: " + result); // Output: 4
+    }
+}
