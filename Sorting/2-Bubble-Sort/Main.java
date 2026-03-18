f90fd01fdbebf11269f034a48183f7c972b86500
Supratim Mandal <supratimm98@gmail.com>
2026-03-18 11:21:23 +0530

    Bubble Sort (updated didSwap integer)

--- Sorting/2-Bubble-Sort/Main.java
+++ Sorting/2-Bubble-Sort/Main.java
@@ -1,15 +1,20 @@
 class BubbleSort{
     void bubbleSort(int[] arr){
         int n = arr.length;
-
+        int didSwap = 0;
         for(int i = n-1; i>=0; i--){
             for(int j= 0; j<=i-1; j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    didSwap = 1;
                 }
             }
+            if(didSwap == 0){
+                break;
+            }
+            System.out.println("Runs");
 
         }
     }
