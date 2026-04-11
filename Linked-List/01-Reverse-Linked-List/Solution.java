9cb693b42f014d33173e2d8a4dcdeb9a0fd7f5e2
Supratim Mandal <supratimm98@gmail.com>
2026-04-11 17:49:16 +0530

    Reverse Linked List

---
+++ Linked-List/01-Reverse-Linked-List/Solution.java
@@ -0,0 +1,68 @@
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+public class Solution {
+    
+    
+    public static Node reverseLinkedList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    // Helper method to print the linked list
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // 1. Manually creating a list: 1 -> 2 -> 3 -> 4 -> 5
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println("Original Linked List:");
+        printList(head);
+
+        // 2. Reverse the list
+        Node reversedHead = reverseLinkedList(head);
+
+        System.out.println("Reversed Linked List:");
+        printList(reversedHead);
+    }
+}
